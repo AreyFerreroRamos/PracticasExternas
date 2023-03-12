f7cdf601f68396185d854562491c321e095fafd3
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-03-12 21:37:58 +0100

    Arey: Es modifica el codi per a que els mimetypes dels fitxers continguts en el fitxer comprimit es guardin a un arxiu anomenat data.txt .

--- get_data.sh
+++ get_data.sh
@@ -4,24 +4,21 @@
 # Date: March 11, 2023. Version: 1.
 # Description:.
 #	Paramethers:
-#		- Compressed file with the data that is need to be processed.
+#		-A compressed file with the data that is need to be processed.
 #	Output:
-#		-
+#		-A file that contains the mimetypes of all the files that are in the compressed file that is given by parameter.
 
 if [ $# -eq 1 ]
 then
 	if [ $(echo $1 | cut -f2 -d'.') = 'spring' ]
 	then
 		dir_name=$(echo $1 | cut -f1 -d'.')
-		if [ ! -d $dir_name ]
-		then
-			mkdir $dir_name
-			tar -xf $1 -C $dir_name
-		fi
+		mkdir $dir_name
+		tar -xf $1 -C $dir_name
 		IFS=$'\n'
 		for file_name in $(ls -l $dir_name | tail -n +2 | tr -s ' ' | cut -f9 -d' ')
 		do
-			file --mime-type $dir_name/$file_name
+			file --mime-type $dir_name/$file_name >> data.txt
 		done
 		exit 0
 	else
