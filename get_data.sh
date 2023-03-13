25b53a8ea272fd946df958e658a47dfc50fc8e4c
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-03-13 11:34:30 +0100

    Arey: Reestructuració de la secció del codi encarregada de la detecció d'errors en l'entrada de l'script per a seguir el format estàndard de les cláusules de guarda.

--- get_data.sh
+++ get_data.sh
@@ -4,28 +4,28 @@
 # Date: March 11, 2023. Version: 1.
 # Description:.
 #	Paramethers:
-#		-A compressed file with the data that is need to be processed.
+#		-A compressed file with the extension '.spring' (spring compressor) with the data that is need to be processed.
 #	Output:
 #		-A file that contains the mimetypes of all the files that are in the compressed file that is given by parameter.
 
-if [ $# -eq 1 ]
+if [ $# -ne 1 ]
 then
-	if [ $(echo $1 | cut -f2 -d'.') = 'spring' ]
-	then
-		dir_name=$(echo $1 | cut -f1 -d'.')
-		mkdir $dir_name
-		tar -xf $1 -C $dir_name
-		IFS=$'\n'
-		for file_name in $(ls -l $dir_name | tail -n +2 | tr -s ' ' | cut -f9 -d' ')
-		do
-			file --mime-type $dir_name/$file_name >> data.txt
-		done
-		exit 0
-	else
-		echo -e "Error: The input must be a compressed file with the extension '.spring' (spring compressor)." >&2
-		exit 1
-	fi
-else
-	echo -e "Error: The number of parameters is incorrect. A compressed file is need to be given as parameter." >&2
+	echo -e "Error: The number of parameters is incorrect. The only parameter has to be a compressed file." >&2
+	exit 1
+fi
+
+if [ $(echo $1 | cut -f2 -d'.') != 'spring' ]
+then
+	echo -e "Error: The input must be a compressed file with the extension '.spring' (spring compressor)." >&2
 	exit 2
 fi
+
+dir_name=$(echo $1 | cut -f1 -d'.')
+mkdir $dir_name
+tar -xf $1 -C $dir_name
+IFS=$'\n'
+for file_name in $(ls -l $dir_name | tail -n +2 | tr -s ' ' | cut -f9 -d' ')
+do
+	file --mime-type $dir_name/$file_name >> data.txt
+done
+exit 0
