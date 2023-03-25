e38cf80da01c6ff89d984a1e5e321fd1cfc826ed
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-03-25 18:34:08 +0100

    Arey: aVANCES CÓDIGO ALPHA DIVERSITY.

--- alpha_diversity.py
+++ alpha_diversity.py
@@ -11,11 +11,22 @@ import pandas as pd
 import sys
 import os
 
-if len(sys.argv) == 2:
-    if os.path.isfile(sys.argv[1]):
-        file = pd.read_csv(sys.argv[1], sep=' ')
-        print(file)
-    else:
-        print("Error: The input must be a file corresponding to a species of vertebrate.")
-else:
+if len(sys.argv) != 2:
     print("Error: The number of parameters is incorrect. The only parameter has to be a file.")
+    exit()
+
+if not os.path.isfile(sys.argv[1]):
+    print("Error: The input must be a file corresponding to a species of vertebrate.")
+    exit()
+
+individuals=[]
+df_vertebrate = pd.read_table(sys.argv[1], delimiter=' ', header=0)
+
+for individual in df_vertebrate:
+    num_bacterial_species_per_individual = 0
+    for num_bacterial_species_per_genus in df_vertebrate[individual]:
+        num_bacterial_species_per_individual += num_bacterial_species_per_genus
+    individuals.append(num_bacterial_species_per_individual)
+
+print("alpha_diversity: "+str(sum(individuals) / len(individuals)))
+   
