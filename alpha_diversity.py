9eb74cffcee2121ca45c0d4de572ad449c866cfa
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-03-28 18:17:08 +0200

    Arey: Rediseny del procediment per calcular l'alpha diversity, ja que no era correcte. Ara, l'alpha diversity es calcula per cada individu i l'index de Shannon s'aplica al nombre d'especies per gènere bacterià en un individu.

--- alpha_diversity.py
+++ alpha_diversity.py
@@ -32,32 +32,36 @@ if not os.path.isfile(sys.argv[2]):
 df_vertebrate = pd.read_table(sys.argv[1], delimiter=' ', header=0)
 df_metadata = pd.read_table(sys.argv[2], delimiter=';', header=0)
 
-sample_types = ['Wild', 'Captivity']
 alpha_diversities_wild = []
 alpha_diversities_captivity = []
 
-for individual in df_vertebrate:
+for individual in df_vertebrate: 
     num_bacterial_species_per_individual = 0
     for num_bacterial_species_per_genus in df_vertebrate[individual]:
         num_bacterial_species_per_individual += num_bacterial_species_per_genus
+    alpha_diversity = 0
+    for num_bacterial_species_per_genus in df_vertebrate[individual]:
+        if num_bacterial_species_per_genus != 0:
+            alpha_diversity += num_bacterial_species_per_genus / num_bacterial_species_per_individual * math.log(num_bacterial_species_per_genus / num_bacterial_species_per_individual)
+    alpha_diversity = - alpha_diversity
+    
     row = 0
     for sample in df_metadata[df_metadata.columns[0]]:
         if sample == individual:
             sample_type = df_metadata.loc[row, df_metadata.columns[4]]
         else:
             row += 1
+    
     if sample_type == "Wild":
-        alpha_diversities_wild.append(num_bacterial_species_per_individual * math.log(num_bacterial_species_per_individual))
+        alpha_diversities_wild.append(alpha_diversity)
     else:
-        alpha_diversities_captivity.append(num_bacterial_species_per_individual * math.log(num_bacterial_species_per_individual))
-
-print("Alpha_diversity wild: "+str(round( - sum(alpha_diversities_wild) / len(alpha_diversities_wild)))+" species.")
-print("Alpha_diversity captivity: "+str(round( - sum(alpha_diversities_captivity) / len(alpha_diversities_captivity)))+" species.")
+        alpha_diversities_captivity.append(alpha_diversity)
 
 alpha_diversities = [alpha_diversities_wild, alpha_diversities_captivity]
+sample_types = ['Wild', 'Captivity']
 
-plt.boxplot(alpha_diversities, labels=sample_types, patch_artist=True)
-plt.xlabel("sample type")
-plt.ylabel("alpha diversity")
 plt.title("Bacterial diversity in animal species")
+plt.xlabel("Sample type")
+plt.ylabel("Alpha diversity")
+plt.boxplot(alpha_diversities, labels=sample_types, patch_artist=True)
 plt.show()
