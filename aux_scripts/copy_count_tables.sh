a2a35285229f6828dc05380747b0946c4fb684fc
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-03-29 14:25:13 +0200

    Arey: Actualització del script per copiar totes les taules amb la informació de tipus 'count_genus' per a que copiï totes les taules amb la informació de qualsevol tipus que hi hagi al directori segons s'especifiqui per paràmetre.

--- aux_scripts/copy_count_tables.sh
+++ aux_scripts/copy_count_tables.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
 
 # Author: Arey Ferrero Ramos.
-# Data: March 28, 2023. Version: 1.
-# Description: Given a directory, this script copys all the 'count_genus' files of that directory in a local directory inside the project."
+# Data: March 28, 2023. Version: 2.
+# Description: Given a directory, this script copys all the files from that directory which have the information in wich we are interested (specified by parameter) in a local directory inside our project.
 # 	Input:
 #		-A directory with a list of files.
+#		-The type of information in which we are interested.
 #	Output:
-#		-A directory with all the 'count_genus' files from the input directory.
+#		-A directory with all the files from the input directory that contains the information in which we are interested.
 
 if [ $# -eq 0 ]
 then
-	echo -e "copy_count_tables.sh: $0 directory" >&2
+	echo -e "copy_count_tables.sh: $0 directory search_information" >&2
 	exit 1
 fi
 
-if [ $# -gt 1 ]
+if [ $# -gt 2 ]
 then
-	echo -e "Error: The number of parameters is incorrect. The input must be only one directory with a list of files." >&2
+	echo -e "Error: The number of parameters is incorrect. The input must be a directory with a list of files and the information that indicated in which files we are interested in." >&2
 	exit 2
 fi
 
 if [ ! -d "$1" ]
 then
-	echo -e "Error: The input is not a directory. It must be a directory with a list of files." >&2
+	echo -e "Error: The first input is not a directory. It must be a directory with a list of files." >&2
 	exit 3
 fi
 
 if [ -z "$(ls -A -- "$1")" ]
 then
-	echo -e "Error: The input directory is empty. The directory must contains a list of files." >&2
+	echo -e "Error: The first input is a directory, but it is empty. The directory must contains a list of files." >&2
 	exit 4
 fi
 
-mkdir -p ../Tables
+if [ -z "$2" ]
+then
+	echo -e "Error: The second input is empty. It must be a string with the information that indicated in which files of the directory we are interested in." >&2
+	exit 5
+fi
+
+mkdir -p ../Tables/$2
 
 for file_name in "$1"/*
 do
-	if [ ! -z $(echo "$file_name" | grep -E ".*count_genus.*") ]
+	if [ ! -z $(echo "$file_name" | grep -E ".*$2.*") ]
 	then
-		cp -p "$file_name" ../Tables 
+		cp -p "$file_name" ../Tables/$2
 	fi
 done
 
