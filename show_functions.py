05ee5432e5b8aa9e2417b6c7d3d5b56017b1b113
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-04-20 18:10:46 +0200

    Arey: S'afegeix separació entre la diferent informació que s'imprimeix per pantalla.

--- show_functions.py
+++ show_functions.py
@@ -11,11 +11,11 @@ def alpha_diversities_sample_type(alpha_diversities, sample_type):
     print(sample_type)
     for specie in alpha_diversities:
         print(specie, alpha_diversities[specie][sample_type])
+    print()
 
 
 def alpha_diversities(alpha_diversities):
     alpha_diversities_sample_type(alpha_diversities, 'Wild')
-    print()
     alpha_diversities_sample_type(alpha_diversities, 'Captivity')
 
 
