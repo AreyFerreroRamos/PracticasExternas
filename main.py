c690e688f561d81d2d1a8baad6a12d983ba2fb95
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-05-12 16:32:19 +0200

    Arey: Renombrat dels métodes per emmagatzemar les dades del fitxer d'entrada, no només en funció de la informació que emmagatzemen, sinó també en funció del tipus d'estructures de dades en que s'emmagatzemen.

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if not os.path.isfile(sys.argv[3]):
     exit()
 
 
-def relative_abundances_bacterial_genus(df_vertebrates):
+def relative_abundances_bacterial_genus_list(df_vertebrates):
     relative_abundances = {}
     for bacterial_genus in df_vertebrates.index:
         relative_abundances[bacterial_genus] = 0
@@ -51,14 +51,14 @@ def relative_abundances_bacterial_genus(df_vertebrates):
     return num_zeros / num_abundances, relative_abundances
 
 
-def alpha_diversities(df_vertebrates, df_metadata):
-    alpha_diversities_individual = {}
+def alpha_diversities_list(df_vertebrates, df_metadata):
+    alpha_diversities = {}
 
     for individual in df_vertebrates:
         specie, sample_type = support.get_specie_sample_type(individual, df_metadata)
 
-        if specie not in alpha_diversities_individual:
-            alpha_diversities_individual[specie] = {'Wild': [], 'Captivity': []}
+        if specie not in alpha_diversities:
+            alpha_diversities[specie] = {'Wild': [], 'Captivity': []}
 
         num_bacterial_species_per_individual = support.get_num_species_per_individual(individual, df_vertebrates)
 
@@ -68,13 +68,13 @@ def alpha_diversities(df_vertebrates, df_metadata):
             if relative_abundance != 0:
                 alpha_diversity += relative_abundance * math.log(relative_abundance)
 
-        alpha_diversities_individual[specie][sample_type].append(0 - alpha_diversity)
+        alpha_diversities[specie][sample_type].append(0 - alpha_diversity)
 
-    return alpha_diversities_individual
+    return alpha_diversities
 
 
-def matrix_abundances_individuals(df_vertebrates):
-    matrix_individuals = np.empty((df_vertebrates.shape[1], df_vertebrates.shape[0]))
+def abundances_individuals_matrix(df_vertebrates):
+    individuals_matrix = np.empty((df_vertebrates.shape[1], df_vertebrates.shape[0]))
 
     num_individuals = 0
     for individual in df_vertebrates:
@@ -83,16 +83,16 @@ def matrix_abundances_individuals(df_vertebrates):
         column_genus = 0
         for num_bacterial_species_per_genus in df_vertebrates[individual]:
             relative_abundance = num_bacterial_species_per_genus / num_bacterial_species_per_individual
-            matrix_individuals[num_individuals][column_genus] = relative_abundance
+            individuals_matrix[num_individuals][column_genus] = relative_abundance
             column_genus += 1
 
         num_individuals += 1
 
-    return matrix_individuals
+    return individuals_matrix
 
 
-def matrix_abundances_vertebrates(df_vertebrates):
-    matrix_vertebrates = np.zeros((2, df_vertebrates.shape[0]))
+def abundances_vertebrates_matrix(df_vertebrates):
+    vertebrates_matrix = np.zeros((2, df_vertebrates.shape[0]))
     specie_ant, _ = support.get_specie_sample_type(df_vertebrates.columns[0], df_metadata)
 
     num_species = num_wild = num_captivity = 0
@@ -100,10 +100,10 @@ def matrix_abundances_vertebrates(df_vertebrates):
         specie, sample_type = support.get_specie_sample_type(individual, df_metadata)
 
         if specie_ant != specie:
-            calculation.normalize_matrix_vertebrates(matrix_vertebrates, num_species, num_wild, num_captivity)
+            calculation.normalize_matrix_vertebrates(vertebrates_matrix, num_species, num_wild, num_captivity)
             num_species += 2
             num_wild = num_captivity = 0
-            matrix_vertebrates.resize((matrix_vertebrates.shape[0] + 2, matrix_vertebrates.shape[1]))
+            vertebrates_matrix.resize((vertebrates_matrix.shape[0] + 2, vertebrates_matrix.shape[1]))
             specie_ant = specie
 
         num_bacterial_species_per_individual = support.get_num_species_per_individual(individual, df_vertebrates)
@@ -111,7 +111,7 @@ def matrix_abundances_vertebrates(df_vertebrates):
         column_genus = 0
         for num_bacterial_species_per_genus in df_vertebrates[individual]:
             relative_abundance = num_bacterial_species_per_genus / num_bacterial_species_per_individual
-            matrix_vertebrates[num_species + support.offset(sample_type)][column_genus] += relative_abundance
+            vertebrates_matrix[num_species + support.offset(sample_type)][column_genus] += relative_abundance
             column_genus += 1
 
         if sample_type == "Wild":
@@ -119,8 +119,8 @@ def matrix_abundances_vertebrates(df_vertebrates):
         else:
             num_captivity += 1
 
-    calculation.normalize_matrix_vertebrates(matrix_vertebrates, num_species, num_wild, num_captivity)
-    return matrix_vertebrates
+    calculation.normalize_matrix_vertebrates(vertebrates_matrix, num_species, num_wild, num_captivity)
+    return vertebrates_matrix
 
 
 df_vertebrates = pd.read_table(sys.argv[1], delimiter=' ', header=0)
@@ -129,37 +129,37 @@ df_metadata = pd.read_table(sys.argv[2], delimiter=';', header=0)
 ploter = show.select_ploter('seaborn')
 
 if sys.argv[4] == "histogram":
-    zeros_ratio, relative_abundances = relative_abundances_bacterial_genus(df_vertebrates)
+    zeros_ratio, relative_abundances_list = relative_abundances_bacterial_genus_list(df_vertebrates)
     print("Total zeros: " + str(round(zeros_ratio * 100, 2)) + "%.")
-    ploter.histogram(support.to_array(relative_abundances))
+    ploter.histogram(support.to_array(relative_abundances_list))
 
 elif sys.argv[4] == "alpha-diversities" or sys.argv[4] == "boxplot":
-    alpha_diversities = alpha_diversities(df_vertebrates, df_metadata)
+    alpha_diversities_list = alpha_diversities_list(df_vertebrates, df_metadata)
     if sys.argv[4] == "alpha-diversities":
-        ploter.alpha_diversities(alpha_diversities)
+        ploter.alpha_diversities(alpha_diversities_list)
     else:
-        calculation.t_test(alpha_diversities)
-        ploter.boxplot(alpha_diversities, sys.argv[3])
+        calculation.t_test(alpha_diversities_list)
+        ploter.boxplot(alpha_diversities_list, sys.argv[3])
 
 else:
     if sys.argv[4] == "individuals":
-        matrix_abundances = matrix_abundances_individuals(df_vertebrates)
+        abundances_matrix = abundances_individuals_matrix(df_vertebrates)
     elif sys.argv[4] == "vertebrates":
-        matrix_abundances = matrix_abundances_vertebrates(df_vertebrates)
+        abundances_matrix = abundances_vertebrates_matrix(df_vertebrates)
 
     if sys.argv[5] == "dendrogram":
-        ploter.dendrogram(matrix_abundances, sys.argv[4])
+        ploter.dendrogram(abundances_matrix, sys.argv[4])
 
     elif sys.argv[5] == "heatmap":
-        ploter.heatmap(matrix_abundances)
+        ploter.heatmap(abundances_matrix)
 
     elif sys.argv[5].split('-')[0] == "clustermap":
         if sys.argv[5].split('-')[1] == "log":
-            calculation.log_matrix(matrix_abundances)
-            ploter.cluster_map(matrix_abundances, '')
+            calculation.log_matrix(abundances_matrix)
+            ploter.cluster_map(abundances_matrix, '')
         elif sys.argv[5].split('-')[1] == "discrete":
-            calculation.discretize_matrix(matrix_abundances, 0.0001)
-            ploter.cluster_map(matrix_abundances, 'viridis')
+            calculation.discretize_matrix(abundances_matrix, 0.0001)
+            ploter.cluster_map(abundances_matrix, 'viridis')
         elif sys.argv[5].split('-')[1] == "fold":
-            matrix_log_fold = calculation.generate_log_fold_matrix(matrix_abundances)
+            matrix_log_fold = calculation.generate_log_fold_matrix(abundances_matrix)
             ploter.cluster_map(matrix_log_fold, 'RdBu')
