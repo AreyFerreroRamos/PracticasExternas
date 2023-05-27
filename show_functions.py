5e601470c2173ef00dc8c21401a5bd5e05e19272
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-05-27 20:13:23 +0200

    Arey: Esborrat d'etiquetat innecessari.

--- show_functions.py
+++ show_functions.py
@@ -252,7 +252,7 @@ class PyplotPloter(Ploter):
 
         i = 0
         while i < len(average_alpha['Wild']):
-            wild_sp.append(ax_scatter.scatter(average_alpha['Wild'][i], average_distance['Wild'][i], label=labels['Wild'][i]))
+            wild_sp.append(ax_scatter.scatter(average_alpha['Wild'][i], average_distance['Wild'][i]))
             i += 1
 
         legend_wild = ax_scatter.legend(handles=wild_sp, labels=labels['Wild'], loc='upper right', borderaxespad=0, ncol=2)
@@ -260,7 +260,7 @@ class PyplotPloter(Ploter):
 
         i = 0
         while i < len(average_alpha['Captivity']):
-            captivity_sp.append(ax_scatter.scatter(average_alpha['Captivity'][i], average_distance['Captivity'][i], label=labels['Captivity'][i]))
+            captivity_sp.append(ax_scatter.scatter(average_alpha['Captivity'][i], average_distance['Captivity'][i]))
             i += 1
 
         legend_captivity = Legend(parent=ax_scatter, handles=captivity_sp, labels=labels['Captivity'], loc='lower left', borderaxespad=0, ncol=2)
