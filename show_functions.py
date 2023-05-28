c8ecb9e311038bf07b751a86a271f66847ddab28
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-05-28 13:40:59 +0200

    Arey: Utilització d'un mecanisme propi de Python per a recórrer les llistes. Això ha permés optimitzar o, com a mínim, reduir el tamany del codi encarregat de generar l'scatterplot que mostra la correlació entre les distàncies i les diversitats alfa de les espècies de vertebrat utilitzant el módul pyplot de la llibreria matplotlib.

--- show_functions.py
+++ show_functions.py
@@ -253,20 +253,15 @@ class PyplotPloter(Ploter):
 
         i = 0
         while i < len(average_alpha):
-            sp.append(ax_scatter.scatter(average_alpha[i], average_distance[i], label=labels[i],
-                                         color=colors[int(i / 2)], alpha=1.0))
+            sp.append(ax_scatter.scatter(average_alpha[i], average_distance[i], color=colors[int(i / 2)], alpha=1.0))
+            sp.append(ax_scatter.scatter(average_alpha[i + 1], average_distance[i + 1], color=colors[int(i / 2)],
+                                         alpha=0.5))
             i += 2
 
-        legend_wild = ax_scatter.legend(handles=sp[0:25], labels=labels[::2], loc='lower left', ncol=2)
+        legend_wild = ax_scatter.legend(handles=sp[::2], labels=labels[::2], loc='lower left', ncol=2)
         plt.setp(legend_wild.get_texts(), fontsize=7)
 
-        i = 1
-        while i < len(average_alpha):
-            sp.append(ax_scatter.scatter(average_alpha[i], average_distance[i], label=labels[i],
-                                         color=colors[int(i / 2)], alpha=0.5))
-            i += 2
-
-        legend_captivity = Legend(parent=ax_scatter, handles=sp[25:50], labels=labels[1::2], loc='upper right', ncol=2)
+        legend_captivity = Legend(parent=ax_scatter, handles=sp[1::2], labels=labels[1::2], loc='upper right', ncol=2)
         plt.setp(legend_captivity.get_texts(), fontsize=7)
         ax_scatter.add_artist(legend_captivity)
 
