3dda0b39f0653dc262a23f8605adc1e71d79cd39
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-06-04 14:33:36 +0200

    Arey: Versió definitiva de la creació de les matrius d'abundàncies de gèneres bacterians en individus de les espècies Ailuropoda melanoleuca i Peromyscus maniculatus i de l'assaig d'imbricació per aquestes matrius.

--- main.py
+++ main.py
@@ -231,7 +231,6 @@ else:
     elif sys.argv[4].split(' ')[2] == "Captive":
         abundances_matrix = abundances_matrices_specie_sample_type(support.get_code_specie(
             sys.argv[4].split(' ')[0] + ' ' + sys.argv[4].split(' ')[1], sys.argv[3]), 'Captivity', df_vertebrates)
-    print(abundances_matrix.shape[0], abundances_matrix.shape[1])
 
 
 ploter = show.select_ploter('pyplot')
