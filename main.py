c2c3557489ababbd8e64c4d727588ce835489de5
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-06-07 11:06:27 +0200

    Arey: Comentat de la matriu de proves per avaluar l'assaig de nestedness.

--- main.py
+++ main.py
@@ -98,6 +98,6 @@ elif sys.argv[5].split(' ')[0] == "clustermap":
         matrix_log_fold = calculation.generate_log_fold_matrix(abundances_matrix)
         ploter.cluster_map(matrix_log_fold, 'RdBu')
 
-matrix = np.array([[1, 1, 1, 0], [1, 1, 1, 1], [1, 0, 0, 0], [1, 1, 0, 0]])
-nested_matrix, p_value = calculation.nestedness_assessment(matrix, 1000)
-print(nested_matrix, p_value)
+# matrix = np.array([[1, 1, 1, 0], [1, 1, 1, 1], [1, 0, 0, 0], [1, 1, 0, 0]])
+# nested_matrix, p_value = calculation.nestedness_assessment(matrix, 1000)
+# print(nested_matrix, p_value)
