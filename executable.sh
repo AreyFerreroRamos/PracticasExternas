a1e031a7fda1ab1680e0a414c7a32813aae120fe
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2023-10-10 11:19:38 +0200

    Arey: Modificació de les sortides d'error en l'script per executar el projecte.

--- executable.sh
+++ executable.sh
@@ -12,25 +12,25 @@
 if [ ! -d input_files ]
 then
 	echo -e "Error: The directory input_files that contains the input data for the project doesn't exist." >&2
-	exit 2
+	exit 1
 fi
 
 if [ ! -f input_files/count_Genus_all.tsv ]
 then
 	echo -e "Error: The file count_Genus_all.tsv, that contains the diversity of the individuals of all the vertebrate species in the study, doesn't exist." >&2
-	exit 3
+	exit 2
 fi
 
 if [ ! -f input_files/metadata.csv ]
 then
 	echo -e "Error: The file metadata.csv, that contains the metadata of all the samples in the study, doesn't exist." >&2
-	exit 4
+	exit 3
 fi
 
 if [ ! -f input_files/sp_code.txt ]
 then
 	echo -e "Error: The file sp_code.txt, that contains a list of the scientific name that corresponds to the code that identifies a vertebrate species, doesn't exist" >&2
-	exit 5
+	exit 4
 fi
 
 python3 main.py input_files/count_Genus_all.tsv input_files/metadata.csv input_files/sp_code.txt "$1" "$2" "$3"
